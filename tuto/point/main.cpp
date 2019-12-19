fb5db8b629e47497d2656b3a4bb7087a07700269
luningcowboy <luningcowboy@gmail.com>
2019-12-19 20:54:08 +0800

    字符串与指针

--- tuto/point/main.cpp
+++ tuto/point/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main(){
     int p1 = 1;
@@ -17,5 +18,14 @@ int main(){
     for(int j = 0; j < 3; j++){
         cout<<*(p[j])<<","<<*(p[j] + 1)<<","<<*(p[j] + 2)<<","<<*(p[j] + 3)<<endl;
     }
+
+    char str[] = "I am a programmer.";
+    char * str1 = "abc";
+    char * str2[] = {"Hello world", "Good Bye"};
+    string str3 = "I am a programmer, too.";
+    cout<<"str: "<<str<<endl;
+    cout<<"str1: "<<str1<<endl;
+    cout<<"str2[0]: "<<str2[0]<<endl;
+    cout<<"str3: "<<str3<<endl;
     return 0;
 }
