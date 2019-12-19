a339424690a7dfb1e429694d84632276509ba004
luningcowboy <luningcowboy@gmail.com>
2019-12-19 20:51:18 +0800

    指针数组，数组指针

--- tuto/point/main.cpp
+++ tuto/point/main.cpp
@@ -9,5 +9,13 @@ int main(){
     cout<<p1<<" "<<*p2<<endl;
     *p2 = 3;
     cout<<p1<<" "<<*p2<<endl;
+    int *p[3];
+    int a[3][4] = {1,2,3,4,5,6,7,8,9,10, 11, 12};
+    for(int i = 0; i < 3; i++){
+        p[i] = a[i];
+    }
+    for(int j = 0; j < 3; j++){
+        cout<<*(p[j])<<","<<*(p[j] + 1)<<","<<*(p[j] + 2)<<","<<*(p[j] + 3)<<endl;
+    }
     return 0;
 }
