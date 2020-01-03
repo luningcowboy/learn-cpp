46fd990a8dd32ac7819040f1629f56dbb07dc8cd
luningcowboy <luningcowboy@gmail.com>
2020-01-03 13:38:40 +0800

    add enum

---
+++ tuto/test-enum/CMakeFiles/test-enum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test-enum.dir/main.cpp.o"
+  "test-enum.pdb"
+  "test-enum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test-enum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
